c5ae50e4c504aca7f3636ab17a7aaa5997a4182d
Pedro Previatti <pvlpreviatti@gmail.com>
2024-02-08 09:00:54 -0300

    first commit

---
+++ api-grade/src/test/java/com/ltp/apigrade/ApiGradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltp.apigrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
