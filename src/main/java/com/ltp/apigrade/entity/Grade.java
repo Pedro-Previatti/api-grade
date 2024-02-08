1f94b9f5758fa01afe4461d249bbb772f3cc82ac
Pedro Previatti <pvlpreviatti@gmail.com>
2024-02-08 10:04:56 -0300

    creating entities

---
+++ src/main/java/com/ltp/apigrade/entity/Grade.java
@@ -0,0 +1,23 @@
+package com.ltp.apigrade.entity;
+
+public class Grade {
+    
+    private Long id;
+    private String score;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String score() {
+        return this.score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+}
