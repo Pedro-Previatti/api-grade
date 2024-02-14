a124ad09f6ec3dea3eb3e1c6f49a21eb5eb8c5c9
Pedro Previatti <pvlpreviatti@gmail.com>
2024-02-14 10:53:21 -0300

    many to many

--- src/main/java/com/ltp/apigrade/entity/Course.java
+++ src/main/java/com/ltp/apigrade/entity/Course.java
@@ -8,6 +8,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
@@ -47,4 +50,12 @@ public class Course {
     @JsonIgnore
     @OneToMany(mappedBy = "course", cascade = CascadeType.ALL)
     private List<Grade> grades;
+
+    @ManyToMany
+    @JoinTable(
+        name = "course_student",
+        joinColumns = @JoinColumn(name = "course_id"),
+        inverseJoinColumns = @JoinColumn(name = "student_id")
+    )
+    private List<Student> students;
 }
